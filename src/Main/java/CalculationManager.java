d7c667b0a8ea8168c1d522f9db623aec980bd686
Matthew <Matthew.Wright.2018@live.rhul.ac.uk>
2020-10-28 14:25:50 +0000

    Added calculateVar function for calculating 99% confidence single day var only

--- src/Main/java/CalculationManager.java
+++ src/Main/java/CalculationManager.java
@@ -1,7 +1,13 @@
 public class CalculationManager {
 
-  public static double calculateVar(){
-    return 0;
+  public static double calculateVar(double dailyVolatility, double assetValue){
+    double dailyStandardDeviation = assetValue * (dailyVolatility / 100);
+
+    double normSinV = 2.326; //For now we maintain this value i.e. a 99% VaR
+
+    double singleDayVar = normSinV * dailyStandardDeviation;
+
+    return singleDayVar;
   }
 
 }
