3e3226a9d464cf239e78147bd977d9b9018836ce
Matthew <Matthew.Wright.2018@live.rhul.ac.uk>
2020-11-03 16:36:06 +0000

    Added new method to calculate daily volatility, currently calculates it's mean

--- src/Main/java/CalculationManager.java
+++ src/Main/java/CalculationManager.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
@@ -22,14 +23,38 @@ public class CalculationManager {
         // Format: [<symbol>@<YYYY-MM-dd>: low-high, open-close (adjusted close)]
         System.out.println(i + ", " + historicalData.get(i).getAdjClose());
 
-        
+
 
       }
+
+      calculateVolatility(historicalData);
+
     } catch (IOException e) {
       e.printStackTrace();
     }
 
     return singleDayVar;
+  }
+
+  /**
+   * Method for calculating the daily volatility of a stock based on it's historical data.
+   * @param historicalData List of type HistoricalQuote
+   * @return A double value representing the daily volatility of the stock
+   */
+  public static double calculateVolatility(List<HistoricalQuote> historicalData) {
+
+    BigDecimal meanStockPrice = new BigDecimal("0.0");
+
+    // Increment through the data and sum them all up
+    for(int i = 0; i < historicalData.size(); i++) {
+      meanStockPrice = meanStockPrice.add(historicalData.get(i).getAdjClose());
+    }
+
+    meanStockPrice = meanStockPrice.divide(new BigDecimal(historicalData.size()), 2, RoundingMode.UP);
+
+    System.out.println("Mean: " + meanStockPrice);
+
+    return 0.0;
   }
 
 }
