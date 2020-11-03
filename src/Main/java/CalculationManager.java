736c158fa043b0cfb0258115d2035568ed9e147c
Matthew <Matthew.Wright.2018@live.rhul.ac.uk>
2020-11-03 16:16:04 +0000

    Testing new historical data gathering method, checking format of it's output for use

--- src/Main/java/CalculationManager.java
+++ src/Main/java/CalculationManager.java
@@ -1,3 +1,10 @@
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.List;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+import yahoofinance.histquotes.HistoricalQuote;
+
 public class CalculationManager {
 
   public static double calculateVar(double dailyVolatility, double assetValue){
@@ -6,6 +13,17 @@ public class CalculationManager {
     double normSinV = 2.326; //For now we maintain this value i.e. a 99% VaR
 
     double singleDayVar = normSinV * dailyStandardDeviation;
+
+    DataManager data = new DataManager();
+    try {
+      List<HistoricalQuote> historicalData = data.getHistoricalPrices("GOOG");
+
+      for (int i = 0; i < historicalData.size(); i++ ) {
+        System.out.println(historicalData.get(i));
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
 
     return singleDayVar;
   }
