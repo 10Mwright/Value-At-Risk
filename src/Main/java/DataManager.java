0d4150449afd1f95dcd6e2063908632a85d3eec8
Matthew <Matthew.Wright.2018@live.rhul.ac.uk>
2020-11-03 16:14:16 +0000

    Implemented basic method to retrieve one year of data from a defined stock

---
+++ src/Main/java/DataManager.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+import yahoofinance.histquotes.HistoricalQuote;
+import yahoofinance.histquotes.Interval;
+
+public class DataManager {
+
+  /**
+   * Method to retrieve historical stock data from Yahoo Finance via API.
+   * @param tickerSymbol the requested stocks ticker symbol
+   * @return ArrayList of type HistoricalQuote
+   * @throws IOException When a connection error occurs
+   */
+  public static List<HistoricalQuote> getHistoricalPrices(String tickerSymbol) throws IOException {
+    Calendar startDate = Calendar.getInstance();
+    Calendar endDate = Calendar.getInstance();
+    startDate.add(Calendar.YEAR, -1); //We'll initially only consider the previous year of data
+
+    Stock target = YahooFinance.get(tickerSymbol, startDate, endDate, Interval.DAILY);
+
+    List<HistoricalQuote> historical = new ArrayList<HistoricalQuote>();
+
+    historical = target.getHistory();
+
+    System.out.println(historical);
+    return historical;
+  }
+
+}
