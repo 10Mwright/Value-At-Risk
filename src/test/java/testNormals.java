ee5d31498dd54f48acce5d125484e9b3a13132fa
Matthew <Matthew.Wright.2018@live.rhul.ac.uk>
2020-11-05 16:13:35 +0000

    Added test case for new normals object to store NormSinV values

---
+++ src/test/java/testNormals.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import Objects.Normals;
+import org.junit.jupiter.api.Test;
+
+public class testNormals {
+
+  @Test
+  public static void testNormSinV() {
+    assertEquals(2.326347874, Normals.getNormSinV(0.99));
+  }
+}
