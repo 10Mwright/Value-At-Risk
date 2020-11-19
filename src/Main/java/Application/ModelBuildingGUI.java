bf1c7bdb7b589716cccbd58b0b026de0e138f97a
Matthew <Matthew.Wright.2018@live.rhul.ac.uk>
2020-11-19 16:54:23 +0000

    Created initial mockup of simplistic GUI, along with associated class

---
+++ src/Main/java/Application/ModelBuildingGUI.java
@@ -0,0 +1,14 @@
+package Application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class ModelBuildingGUI {
+
+
+
+  @FXML
+  // fx:id="timeHorizonField"
+  private TextField timeHorizonField;
+
+}
