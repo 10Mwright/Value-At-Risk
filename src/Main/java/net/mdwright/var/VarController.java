4aa584e9b18abf43bb2c808e57387fc25d402399
Matthew <Matthew.Wright.2018@live.rhul.ac.uk>
2020-11-20 12:23:49 +0000

    Implemented controller calculate method to use model building first

--- src/Main/java/net/mdwright/var/VarController.java
+++ src/Main/java/net/mdwright/var/VarController.java
@@ -1,6 +1,9 @@
 package net.mdwright.var;
 
+import java.math.BigDecimal;
+import net.mdwright.var.application.ViewInterface;
 import net.mdwright.var.objects.Model;
+import net.mdwright.var.objects.Position;
 
 /**
  * Controller class for Var Calculations. Note: influence from Calculator code 2nd Year software
@@ -12,10 +15,22 @@ public class VarController {
 
   private boolean isModelBuilding;
   private VarModel model = new VarModel();
+  private ViewInterface view = null;
+
+  public VarController(ViewInterface view) {
+    this.view = view;
+    view.addCalcObserver(this::calculateVar); //Set observer to calculateVar method
+  }
 
   public void calculateVar() {
     //Get data from view
+    Position[] portfolio = view.getPortfolio();
+    int timeHorizon = view.getTimeHorizon();
+    double probability = view.getProbability();
 
+    BigDecimal valueAtRisk = model.calculateVar(portfolio, timeHorizon, probability);
+
+    view.setResult(valueAtRisk);
     //Pass to VarModel's method
   }
 
