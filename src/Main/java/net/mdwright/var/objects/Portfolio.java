35df79571d24612af7b3a4f3dc59140886ab0442
Matthew <Matthew.Wright.2018@live.rhul.ac.uk>
2020-11-27 18:50:36 +0000

    Finished clean-up & JavaDocs of object classes

--- src/Main/java/net/mdwright/var/objects/Portfolio.java
+++ src/Main/java/net/mdwright/var/objects/Portfolio.java
@@ -13,34 +13,45 @@ public class Portfolio {
   private Scenario[] scenarios;
   private BigDecimal valueAtRisk;
 
-  public Portfolio() {
-
-  }
-
+  /**
+   * Constructor method for creating a portfolio with multiple Positions.
+   * @param positions Array of type Position containing all positions within the portfolio
+   */
   public Portfolio(Position[] positions) {
     this.positions = positions;
   }
 
+  /**
+   * Constructor method for creating a portfolio with a single Position.
+   * @param position A Position object representing the desired position
+   */
   public Portfolio(Position position) {
+    //TODO: validation to ensure portfolio has a single position
     this.positions = new Position[]{position};
   }
 
-  public Position[] getPositions() {
-    return positions;
-  }
-
+  /**
+   * Method for retrieving a specific Position from a portfolio of multiple positions.
+   * @param positionIndex An int value representing the index of the desired position within the array
+   * @return The Position object representing the desired position
+   */
   public Position getPosition(int positionIndex) {
+    //TODO: validation to ensure portfolio contains a valid array
     return positions[positionIndex];
   }
 
+  /**
+   * Method for retrieving the size of the positions array.
+   * @return An int value representing the size of the array
+   */
   public int getSize() {
     return positions.length;
   }
 
-  public Scenario[] getScenarios() {
-    return scenarios;
-  }
-
+  /**
+   * Method for setting the scenarios array during historical sim calculations.
+   * @param scenarios An array of type Scenario containing all scenarios for this portfolio
+   */
   public void setScenarios(Scenario[] scenarios) {
     this.scenarios = scenarios;
   }
@@ -48,15 +59,19 @@ public class Portfolio {
   /**
    * Method for returning a sorted array of scenarios.
    *
-   * @return Array of type Scenario containing a sorted array by value Code adapted from
+   * @return Array of type Scenario containing a sorted array by value (Descending)
+   *
+   * Code adapted from:
    * https://stackoverflow.com/questions/33462923/sort-elements-of-an-array-in-ascending-order
    */
   public Scenario[] sortScenarios() {
     Scenario temp;
 
+    //Runs through each scenario and the scenarios proceeding it
     for (int i = 0; i <= this.scenarios.length; i++) {
       for (int j = i + 1; j < this.scenarios.length; j++) {
         if (this.scenarios[i] != null && this.scenarios[j] != null) {
+          //Compares the values, if lower then swap around
           if (this.scenarios[i].getValueUnderScenario()
               .compareTo(this.scenarios[j].getValueUnderScenario()) < 0) {
             temp = this.scenarios[i];
@@ -69,10 +84,18 @@ public class Portfolio {
     return this.scenarios;
   }
 
+  /**
+   * Method for retrieving the VaR value for this portfolio.
+   * @return A BigDecimal value representing the VaR value
+   */
   public BigDecimal getValueAtRisk() {
     return valueAtRisk;
   }
 
+  /**
+   * Method for setting the VaR value for this portfolio.
+   * @param valueAtRisk A BigDecimal value representing the VaR value
+   */
   public void setValueAtRisk(BigDecimal valueAtRisk) {
     this.valueAtRisk = valueAtRisk;
   }
--- src/Main/java/net/mdwright/var/objects/Scenario.java
+++ src/Main/java/net/mdwright/var/objects/Scenario.java
@@ -3,25 +3,54 @@ package net.mdwright.var.objects;
 import java.math.BigDecimal;
 import java.util.Calendar;
 
+/**
+ * Object for storing information for each scenario between two dates.
+ *
+ * @author Matthew Wright
+ */
 public class Scenario {
 
   private Calendar dateOne, dateTwo;
   private BigDecimal valueUnderScenario;
 
+  /**
+   * Constructor method for constructing a Scenario with to and from dates and it's respective
+   * gain/loss.
+   *
+   * @param dateOne A Calendar object containing the from date
+   * @param dateTwo A Calendar object containing the to date
+   * @param valueUnderScenario A BigDecimal value representing the gain or loss during this
+   * scenarios period
+   */
   public Scenario(Calendar dateOne, Calendar dateTwo, BigDecimal valueUnderScenario) {
     this.dateOne = dateOne;
     this.dateTwo = dateTwo;
     this.valueUnderScenario = valueUnderScenario;
   }
 
+  /**
+   * Method for retrieving the gain or loss that occurred in this scenario.
+   *
+   * @return A BigDecimal representing the gain or loss during this scenarios period
+   */
   public BigDecimal getValueUnderScenario() {
     return valueUnderScenario;
   }
 
+  /**
+   * Method for retrieving the from date of this scenario.
+   *
+   * @return A Calendar object representing the from date
+   */
   public Calendar getDateOne() {
     return dateOne;
   }
 
+  /**
+   * Method for retrieving the to date of this scenario.
+   *
+   * @return A Calendar object representing the to date
+   */
   public Calendar getDateTwo() {
     return dateTwo;
   }
