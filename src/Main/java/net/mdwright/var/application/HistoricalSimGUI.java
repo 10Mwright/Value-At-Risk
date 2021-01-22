37da230f4a864190c83d0a7a96723886afc23655
Matthew <Matthew.Wright.2018@live.rhul.ac.uk>
2021-01-22 13:06:05 +0000

    Duplicated current GUI for historical simulation

---
+++ src/Main/java/net/mdwright/var/application/HistoricalSimGUI.java
@@ -0,0 +1,147 @@
+package net.mdwright.var.application;
+
+import java.math.BigDecimal;
+import java.util.function.Consumer;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
+import net.mdwright.var.objects.Model;
+import net.mdwright.var.objects.Position;
+
+/**
+ * Class for managing GUI for user interactions for historical simulations.
+ * @author Matthew Wright
+ */
+public class HistoricalSimGUI implements ViewInterface {
+
+  @Override
+  public Position[] getPortfolio() {
+    int portfolioSize = portfolioList.getItems().size();
+    Position[] positions = new Position[portfolioSize];
+
+    for (int i = 0; i < portfolioSize; i++) {
+      positions[i] = portfolioList.getItems().get(i);
+    }
+
+    return positions;
+  }
+
+  @Override
+  public Position getNewPosition() {
+    return new Position(tickerSymbolField.getText(), Double.parseDouble(assetValueField.getText()));
+  }
+
+  @Override
+  public int getTimeHorizon() {
+    return Integer.parseInt(timeHorizonField.getText());
+  }
+
+  @Override
+  public double getProbability() {
+    return Double.parseDouble(probabilityField.getText());
+  }
+
+  public int getDataLength() {
+    return Integer.parseInt(dataLengthField.getText());
+  }
+
+  @Override
+  public void setResult(BigDecimal varValue) {
+    resultField.setText(String.valueOf(varValue));
+  }
+
+  @Override
+  public void addNewPosition(Position newPos) {
+    portfolioList.getItems().add(newPos);
+  }
+
+  @Override
+  public void addCalcObserver(Observer obs) {
+    calculateButton.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        obs.tell();
+      }
+    });
+  }
+
+  @Override
+  public void addPortfolioObserver(Observer obs) {
+    addAssetButton.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        obs.tell();
+      }
+    });
+  }
+
+  @Override
+  public void addModelObserver(Consumer<Model> model) {
+    modelSelection.selectedToggleProperty().addListener(
+        new ChangeListener<Toggle>() {
+          @Override
+          public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue,
+              Toggle newValue) {
+            model.accept(
+                newValue == modelBuildingSelection ? Model.ModelBuilding : Model.HistoricalSim);
+          }
+        }
+    );
+  }
+
+  @FXML
+  // fx:id="timeHorizonField"
+  private TextField timeHorizonField;
+
+  @FXML
+  // fx:id="probabilityField"
+  private TextField probabilityField;
+
+  @FXML
+  // fx:id="tickerSymbolField"
+  private TextField tickerSymbolField;
+
+  @FXML
+  // fx:id="assetValueField"
+  private TextField assetValueField;
+
+  @FXML
+  // fx:id="dataLengthField"
+  private TextField dataLengthField;
+
+  @FXML
+  // fx:id="modelBuildingSelection"
+  private RadioButton modelBuildingSelection;
+
+  @FXML
+  // fx:id="historicalSimSelection"
+  private RadioButton historicalSimSelection;
+
+  @FXML
+  // fx:id="modelSelection"
+  private ToggleGroup modelSelection;
+
+  @FXML
+  // fx:id="resultField"
+  private TextField resultField;
+
+  @FXML
+  // fx:id="calculateButton"
+  private Button calculateButton;
+
+  @FXML
+  // fx:id="addAssetButton"
+  private Button addAssetButton;
+
+  @FXML
+  // fx:id="portfolioList"
+  private ListView<Position> portfolioList;
+}
