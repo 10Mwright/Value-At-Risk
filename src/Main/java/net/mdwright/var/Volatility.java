cc0e4f8998869bdf6f75ed1a195ce51bb808600d
Matthew <Matthew.Wright.2018@live.rhul.ac.uk>
2021-03-10 14:55:32 +0000

    Swapped over EWMA method to volatility class

--- src/Main/java/net/mdwright/var/Volatility.java
+++ src/Main/java/net/mdwright/var/Volatility.java
@@ -12,7 +12,7 @@ public class Volatility {
   private static final int divisionScale = 2;
 
   /**
-   * Method for calculating the daily volatility of a stock based on it's historical data.
+   * Simple Method for calculating volatility over a position's time period.
    *
    * @param position Position object of the position to calculate volatility for
    * @return A double value representing the daily volatility of the stock influence from website:
@@ -49,6 +49,52 @@ public class Volatility {
     return dailyVolatility;
   }
 
+  /**
+   * Exponentially Weighted Moving Average model for calculating volatility.
+   * 
+   * @param portfolio Portfolio object containing the position objects to be calculated using
+   * @param index Int value representing the index of the position within the Portfolios array
+   * @param lambda Double value representing the lambda constant to be used in calculation
+   * @return double value representing the volatility of the position over the time period
+   */
+  public static double calculateVolatility(Portfolio portfolio, int index, double lambda) {
+    List<HistoricalQuote> historicalData = portfolio.getPosition(index).getHistoricalData();
+
+    double[][] returns = new double[3][historicalData.size()];
+    double currentWeight = (1 - lambda); //Initial weight
+    System.out.println("INITIAL WEIGHT: " + currentWeight);
+
+    double dailyVolatility = 0;
+
+    for (int i = (historicalData.size() - 2); i >= 0; i--) { //Increment through historical data day by day
+      double currentDay = historicalData.get(i).getAdjClose().doubleValue();
+      double yesterday = historicalData.get(i+1).getAdjClose().doubleValue();
+
+      double returnCurrentDay = currentDay / yesterday;
+      returns[0][i] = Math.log(returnCurrentDay); //The natural log of the return in day i
+      returns[1][i] = Math.pow(returns[0][i], 2); //Daily variance (unweighted)
+
+      returns[2][i] = returns[1][i] * currentWeight; //Weighted variance
+
+      dailyVolatility += returns[2][i]; //Sum all weighted values to get a final volatility
+      currentWeight = currentWeight * (lambda);
+      System.out.println("CURRENT WEIGHT: " + currentWeight);
+    }
+
+    dailyVolatility = Math.sqrt(dailyVolatility);
+
+    System.out.println("Daily Volatility (EWMA): " + dailyVolatility);
+
+    portfolio.getPosition(index).setVolatility(dailyVolatility);
+    return dailyVolatility; //Final volatility value by sqrt
+  }
+
+  /**
+   * Method for calculating the mean close price across a historical data set.
+   *
+   * @param position Position object of the position to calculate the mean across
+   * @return BigDecimal value representing the mean across the entire data set
+   */
   public static BigDecimal calculateMean(Position position) {
     BigDecimal meanStockPrice = new BigDecimal("0.0");
 
